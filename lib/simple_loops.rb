32c324e24e9e10e32469e5c4c22a490490e8bf8b
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-09 22:52:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,3 +17,15 @@ def loop_message_n_times(message, number)
     counter += 1
   end
 end
+
+
+def output_array(array)
+  counter = 0 
+  
+  while counter < array.length do
+    puts message
+    counter += 1
+  end
+end
+
+
