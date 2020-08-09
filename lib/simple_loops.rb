aaf4b42fbae838b3754a2f709cc78387bc445581
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-09 23:07:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -31,10 +31,10 @@ end
 
 def return_string_array(array)
   counter = 0
-  new_array = []
+  new_array
   
   while counter < array.length do
-    array[counter].to_s
+    new_counter << array[counter].to_s
     counter += 1
   end
 end
